57e931ac3718e2145f60fb048b3bb68b3a71cdd5
Linda Seiter <linseiter@gmail.com>
2023-04-04 05:32:51 -0400

    add sandpack components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import { Sandpack } from "@codesandbox/sandpack-react";
+//import { Sandpack } from "@codesandbox/sandpack-react";
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackTests,
+  SandpackCodeEditor,
+  SandpackConsole,
+} from "@codesandbox/sandpack-react";
 
 function App() {
-  return <Sandpack />;
+  //return <Sandpack />;
+  return (
+    <SandpackProvider>
+      <SandpackLayout>
+        <SandpackCodeEditor showLineNumbers showTabs />
+        <SandpackTests />
+        <SandpackConsole resetOnPreviewRestart="true" />
+      </SandpackLayout>
+    </SandpackProvider>
+  );
 }
 
 export default App;
